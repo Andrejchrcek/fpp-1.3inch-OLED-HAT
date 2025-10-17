5af7b879996631271a9cc035f101569090935801
Andrejchrcek <andrej.chrcek@gmail.com>
2025-10-17 16:13:34 +0200

    Update postStart.sh

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PYTHON_SCRIPT="/home/fpp/media/plugins/fpp-oled_remote/oled_remote.py
+PYTHON_SCRIPT="/home/fpp/media/plugins/fpp-oled_remote/oled_remote.py"
 
 python3 "$PYTHON_SCRIPT" &

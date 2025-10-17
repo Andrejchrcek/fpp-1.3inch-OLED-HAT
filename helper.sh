75f369544172effa3224dd32e526e62e06d1bb8c
Andrejchrcek <andrej.chrcek@gmail.com>
2025-10-17 19:37:06 +0200

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -2,6 +2,7 @@
 
 CONFIG_FILE="/home/fpp/media/config/plugin.fpp-oled_remote.json"
 LOG_FILE="/home/fpp/media/logs/fpp-oled_remote.log"
+CLEAR_SCRIPT="/home/fpp/media/plugins/fpp-oled_remote/clear_display.py"
 
 # Skontroluj, či jq je dostupné
 if ! command -v jq &> /dev/null; then
@@ -26,9 +27,11 @@ if [ "$ENABLED" = "1" ]; then
   /usr/bin/python3 /home/fpp/media/plugins/fpp-oled_remote/oled_remote.py >> "$LOG_FILE" 2>&1 &
   echo "OLED Remote script started." >> "$LOG_FILE"
 else
-  # Ak beží, zabi
+  # Ak beží, zabi a vyčisti displej
   if [ -n "$PID" ]; then
     kill "$PID"
-    echo "OLED Remote script stopped." >> "$LOG_FILE"
+    sleep 1
+    echo "OLED Remote script stopped, čistím displej." >> "$LOG_FILE"
+    /usr/bin/python3 "$CLEAR_SCRIPT" >> "$LOG_FILE" 2>&1
   fi
 fi

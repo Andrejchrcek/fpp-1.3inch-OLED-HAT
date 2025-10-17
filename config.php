e73ec7b0dd41220823466000142357b3d941f3a1
Andrejchrcek <andrej.chrcek@gmail.com>
2025-10-17 19:06:43 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,23 +1,69 @@
 <?php
-include_once "/opt/fpp/www/common.php";
-
+// Názov pluginu
 $pluginName = "fpp-oled_remote";
-$logFile = "/home/fpp/media/logs/" . $pluginName . ".log";
-$helperScript = "/home/fpp/media/plugins/" . $pluginName . "/helper.sh";
 
-// Callback funkcia na aplikovanie zmien po uložení
-function SaveConfig($status) {
-    global $helperScript, $logFile;
+// Cesty k súborom
+$configFile = "/home/fpp/media/config/plugin." . $pluginName . ".json";
+$helperScript = "/home/fpp/media/plugins/" . $pluginName . "/helper.sh";
+$logFile = "/home/fpp/media/logs/" . $pluginName . ".log";
+
+// Predvolené nastavenia
+$defaults = [
+    'enabled' => "0",
+    'showBattery' => "1"
+];
+
+// Spracovanie uloženia formulára
+if (isset($_POST['save'])) {
+    $newSettings['enabled'] = isset($_POST['enabled']) ? "1" : "0";
+    $newSettings['showBattery'] = isset($_POST['showBattery']) ? "1" : "0";
+
+    file_put_contents($configFile, json_encode($newSettings, JSON_PRETTY_PRINT));
+    
+    // Spustíme helper.sh, aby aplikoval zmeny
     exec("sudo " . $helperScript . " >> " . $logFile . " 2>&1");
+}
+
+// Načítanie aktuálnych nastavení
+$settings = $defaults;
+if (file_exists($configFile)) {
+    $loadedSettings = json_decode(file_get_contents($configFile), true);
+    if (is_array($loadedSettings)) {
+        $settings = array_merge($defaults, $loadedSettings);
+    }
 }
 ?>
 
 <div id="oled-remote-settings" class="settingsGroup">
     <legend>🔌 OLED Remote Control Settings</legend>
 
-    <p><em>When checked, the remote control script will start automatically with FPP after a restart.</em></p>
-    <?php PrintSettingCheckbox("Enable Plugin on Startup", "enabled", $restart = 0, $reboot = 0, "1", "0", $pluginName, $changeCallback = '', $saveCallback = "SaveConfig"); ?>
+    <?php if (isset($_POST['save'])): ?>
+        <div class="alert alert-success">✅ Settings have been saved successfully!</div>
+    <?php endif; ?>
 
-    <p><em>Displays the battery percentage on the OLED screen.</em></p>
-    <?php PrintSettingCheckbox("Show Battery Status", "showBattery", $restart = 0, $reboot = 0, "1", "0", $pluginName, $changeCallback = '', $saveCallback = "SaveConfig"); ?>
+    <form method="POST" action="">
+        <div class="settingsSetting">
+            <label class="col-form-label">Enable Plugin on Startup:</label>
+            <div class="setting">
+                <input type="checkbox" name="enabled" id="enabled" <?php echo $settings['enabled'] === "1" ? 'checked' : ''; ?>>
+                <label for="enabled"><span></span></label>
+                <em>When checked, the remote control script will start automatically with FPP after a restart.</em>
+            </div>
+        </div>
+
+        <div class="settingsSetting">
+            <label class="col-form-label">Show Battery Status:</label>
+            <div class="setting">
+                <input type="checkbox" name="showBattery" id="showBattery" <?php echo $settings['showBattery'] === "1" ? 'checked' : ''; ?>>
+                <label for="showBattery"><span></span></label>
+                <em>Displays the battery percentage on the OLED screen.</em>
+            </div>
+        </div>
+
+        <div class="settingsSetting">
+            <div class="setting">
+                <button type="submit" name="save" class="buttons btn-success">Save Settings</button>
+            </div>
+        </div>
+    </form>
 </div>
